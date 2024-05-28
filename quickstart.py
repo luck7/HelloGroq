9ea5aeca1a17395ad4d3fabacac75a6179cc5e80
Jeff Fox <luck7@live.cn>
2024-05-28 09:46:15 +0800

    add quickstart

---
+++ quickstart.py
@@ -0,0 +1,20 @@
+import os
+
+from groq import Groq
+
+client = Groq(
+    api_key=os.environ.get("GROQ_API_KEY"),
+    # api_key="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",
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Explain the importance of fast language models",
+        }
+    ],
+    model="llama3-8b-8192",
+)
+
+print(chat_completion.choices[0].message.content)
